ffd047cc13ed458f596b5eba794fb1a93672c5db
taichi-kobaya <higoronookonaigadaiji@gmail.com>
2023-02-10 08:54:43 +0900

    first commit

---
+++ src/components/Results.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="results-country" v-if="results.country">{{ country }}</div>
+  <div class="results-city" v-if="results.cityName">{{ cityName }}</div>
+  <div class="results-temp" v-if="results.temperature">{{ temperature }}</div>
+  <div class="results-condition" v-if="results.icon">
+    <img :src="icon" alt="icon">
+    <span>{{ conditionText }}</span>
+  </div>
+</template>
+
+<script setup>
+  import { toRefs } from "vue"
+
+  const props = defineProps({
+    results: Object
+  })
+
+  const { country, cityName, temperature, icon, conditionText } = toRefs(props.results)
+</script>
